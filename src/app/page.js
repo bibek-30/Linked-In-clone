01a0ce8c7fa1d30a40d5e8ed60a7f81c8721950b
bibek-30 <bibekshrestha828@gmail.com>
2024-04-26 09:14:19 +0545

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../components/header';
+import Feed from '../components/homepage/feed';
 
 const page = () => {
   return (
-    <div>
+    <div className='application-outlet'>
       <Header/>
-      Hello world
+      <Feed />
     </div>
   )
 }
